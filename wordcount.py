1f2cf9030131d1027294b54551c72c2a59da6c82
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 13:11:56 -0700

    Now we sort the output list of words & counts by the word's alphabetic order

--- wordcount.py
+++ wordcount.py
@@ -12,13 +12,17 @@ for line in file:
 
     words = line.split()
 
+    # within the line, go through each word one at a time and add to dictionary
+    # or update its count
     for word in words:
         word = word.rstrip(",.-!?")
         word = word.lower()
         word_dict[word] = word_dict.get(word, 0) + 1
 
 
-for word, count in word_dict.items():
+# after we've gone through all lines in the input file, we print out the 
+# total counts for each word, where the words are sorted in alphabetic order
+for word, count in sorted(word_dict.items()):
     print(word, count)
 
 
