d93c0b0f1436b6db09e06b3204a16f51d2b2c918
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 12:31:46 -0700

    Small updates after code review, mainly for style

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,20 @@
 # put your code here.
 def count_words(file_name):
-    word_list = {}
+    word_dict = {}
+
     file = open(file_name)
     for line in file:
-        line.rstrip()
+
         words = line.split()
 
         for word in words:
-            word_list[word]= word_list.get(word, 0) +1
+            word = word.rstrip(",.-!?")
+            word_dict[word] = word_dict.get(word, 0) + 1
 
-    for word, count in word_list.items():
+    for word, count in word_dict.items():
         print(word, count)
 
 
 
-#count_words('test.txt')
-count_words('twain.txt')
+count_words('test.txt')
+#count_words('twain.txt')
